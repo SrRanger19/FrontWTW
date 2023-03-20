18d27f8122eba22f459b51f0264757132a02da5a
Carmen Anel Vilca Mamani <213378@ids.upchiapas.edu.mx>
2023-03-20 12:00:52 -0600

    Registration and login views

--- src/components/Login/Data.jsx
+++ src/components/Login/Data.jsx
@@ -6,29 +6,39 @@ function Data() {
              <br/><br/>
             <div className="title-container-1">
                 <div className="Logo-container">
-                    <img src="/icon/Logo.svg" alt="" />
+                    <img src="/img/logoWTW.png" alt="" />
                 </div>
-               
-                <h1>Touring The Globe✨</h1>
+                <h1 className="abrev-Login">WTW</h1>
+                <h2 className="txt1-Login">Watering The World </h2>
+                <div className="txt2-Login">
+                    <h3>"La necesidad es una oportunidad para inventar"</h3>
+                </div>
+                <div className="Login-Imagen-container">
+                    <img src="/img/Cultivo.jpg" alt="" />
+                </div>
             </div>
 
-            <h3 className='Title_R'>Account login</h3>
-            <form className='LoginForm'>
-                <div>
-                    <label id="Email">Email</label><br />
-                    <input className="place" type="text" name="email" id='email_Login'/>
-                </div>
-                <br />
+            <div className="LoginForm-container">
+                <form className='LoginForm'>
+                    <div className="User-img-container">
+                        <img src="/img/user.png" alt="" />
+                    </div>
+                    <div>
+                        <label id="Email">Correo electrónico:</label><br />
+                        <input className="place" type="text" name="email" id='email_Login'/>
+                    </div>
+                    <br />
 
-                <div >
-                    <label id="Password">Password</label><br />
-                    <input className="place" type="password" name="password" id='password_Login'/>
-                </div>
+                    <div >
+                        <label id="Password">Contraseña:</label><br />
+                        <input className="place" type="password" name="password" id='password_Login'/>
+                    </div>
 
-                <button type='submit' id="Create-User-button">Login</button>
-                <h4 id='Text-Login'>or</h4><br />
-                <Link id="Create-button" to="/Register">Create account</Link>
-            </form>
+                    <button type='submit' id="Login-button">Iniciar sesión</button>
+                    <h4 id='Text-Login'>¿Todavía no tienes una cuenta? <Link id="Text-Register" to="/Register">Regístrate</Link></h4>
+                    
+                </form>
+            </div>
         </div>
     );
 }
